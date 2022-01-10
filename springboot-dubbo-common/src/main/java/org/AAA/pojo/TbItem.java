3ad44ebbd77aea4d88d449352a3403aa53d739d0
bookstoreTest <dubbodemo@protonmail.com>
2022-01-10 22:49:58 +0800

    定义findAll接口

--- springboot-dubbo-common/src/main/java/org/AAA/pojo/TbItem.java
+++ springboot-dubbo-common/src/main/java/org/AAA/pojo/TbItem.java
@@ -1,13 +1,15 @@
-package pojo;
+package org.AAA.pojo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class TbItem {
+public class TbItem implements Serializable {
     private Long id;
     private String title;
     private Double price;
---
+++ springboot-dubbo-common/src/main/java/org/AAA/service/TbItemService.java
@@ -0,0 +1,9 @@
+package org.AAA.service;
+
+import org.AAA.pojo.TbItem;
+
+import java.util.List;
+
+public interface TbItemService {
+    public List<TbItem> findAll();
+}
